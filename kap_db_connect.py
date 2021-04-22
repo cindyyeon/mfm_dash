6073dd29d8479bb586ac752e674ee9fbc333cb2f
cindyyeon <cindy_yeon@naver.com>
2021-04-22 11:36:33 +0900

    Initial commit

---
+++ kap_db_connect.py
@@ -0,0 +1,41 @@
+import pyodbc
+from sqlalchemy import create_engine, MetaData, Table, select
+
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+
+with open("../kap_db_info.txt") as f:
+    lines = f.readlines()
+    server = lines[0].strip()
+    db = lines[1].strip()
+    user = lines[2].strip()
+    pwd = lines[3].strip()
+    dns = lines[4].strip()
+
+class Kapodbc:
+
+    def __init__(self, server=server, db=db, user=user, pwd=pwd):
+        self.server = server
+        self.db = db
+        self.user = user
+        self.pwd = pwd
+
+    def connect(self):
+
+        conn = pyodbc.connect(
+            'DRIVER={SQL Server};SERVER=' + self.server + ';DATABASE=' + self.db + ';UID=' + self.user + ';PWD=' + self.pwd)
+        cursor = conn.cursor()
+
+        return conn, cursor
+
+
+class Kapsqlalchemy:
+    def __init__(self, user=user, pwd=pwd, dns = dns):
+        self.user = user
+        self.pwd = pwd
+        self.dns = dns
+
+    def create_engine(self):
+        conn = create_engine('mssql+pyodbc://' + self.user + ':' + self.pwd + '@' + self.dns)
+        return conn
